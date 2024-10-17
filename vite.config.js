ff196d440ec27060298919993968daee8dd91b61
ivan <ivanacosta0627@gmail.com>
2024-10-17 23:23:38 +0800

    x attempt

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Queueing/',  // This should match your GitHub repository name
   plugins: [react()],
+  base: '/your-repo-name/', // Replace with your actual repository name
 });
