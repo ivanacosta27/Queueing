4dd94fe71def8acdc8271eb314a252df49912e32
ivan <ivanacosta0627@gmail.com>
2024-10-17 23:34:42 +0800

    workplease

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Queueing/', // Replace with your actual repository name
+  base: '/queueing/', // Replace with your actual repository name
 });
